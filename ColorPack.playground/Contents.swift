7a5634a4c338116f83551ecd5ce5860f2c1eed4c
@tottokotkd <tottokotkd@me.com>
2016-10-25 14:45:04 +0900

    Update: playground for v1.1

--- ColorPack.playground/Contents.swift
+++ ColorPack.playground/Contents.swift
@@ -1,19 +1,19 @@
 import ColorPack
 
 // init? (RGB)
-Color(red: 100, green: 200, blue: 50)?.toUIColor
-Color(red: 100, green: 200, blue: 50, alpha: 0.3)?.toUIColor
-Color(red: 100, green: 200, blue: 350)?.toUIColor
-Color(red: 100, green: 200, blue: 50, alpha: 1.3)?.toUIColor
+Color.create(red: 100, green: 200, blue: 50)?.toUIColor
+Color.create(red: 100, green: 200, blue: 50, alpha: 0.3)?.toUIColor
+Color.create(red: 100, green: 200, blue: 350)?.toUIColor
+Color.create(red: 100, green: 200, blue: 50, alpha: 1.3)?.toUIColor
 
 // init? (Hex)
-Color(hex: 0xFFcc44)?.toUIColor
-Color(hex: 0xFFcc44, alpha: 0.6)?.toUIColor
+Color.create(hex: 0xFFcc44)?.toUIColor
+Color.create(hex: 0xFFcc44, alpha: 0.6)?.toUIColor
 
 // init (type safe: RGB, Hex, Web safe)
-Color.RGB(red: .d0, green: .d100, blue: .d180).toUIColor
-Color.Hex(red: .x77, green: .x22, blue: .x99).toUIColor
-Color.WebSafe(red: .k9, green: .kC, blue: .kF).toUIColor
+Color.rgb(red: .d0, green: .d100, blue: .d180).toUIColor
+Color.hex(red: .x77, green: .x22, blue: .x99).toUIColor
+Color.webSafe(red: .k9, green: .kC, blue: .kF).toUIColor
 
 // useful library
 // CSS4: 148 colors
